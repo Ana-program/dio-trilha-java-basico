c251d8d0aa1fed414af9cf644ac01b6fbb926dcc
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2024-05-17 00:33:50 -0300

    Exibindo as informações para o cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -24,6 +24,11 @@ public class ContaTerminal {
         System.out.println("Por favor informe o seu saldo");
         saldo = ler.nextDouble();
 
+        System.out.println("");
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero +  " e seu saldo " + saldo + " já está disponível para saque");
+       
+    ler.close();
+
 }
     }
 
