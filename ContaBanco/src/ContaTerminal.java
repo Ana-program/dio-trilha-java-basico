53d1599abd3cf530f35607e3c0b18c44f77034e9
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2024-05-16 23:16:39 -0300

    Meu projeto Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        
+    }
+    }
+
