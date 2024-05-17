67cfaa0dbc56ebd7c480cac95f6f5eb4f4d19e6a
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2024-05-17 00:01:00 -0300

    Solicitando informações do cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     
     public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
         int numero;
         String agencia;
         String nomeCliente;
         double saldo;
 
-        
-    }
+        System.out.println("Por favor digite o número da sua conta");
+        numero = ler.nextInt();
+        ler.nextLine();
+
+        System.out.println("Por favor digite o número da sua agência com o digito");
+        agencia = ler.nextLine();
+
+        System.out.println("Por favor informe o seu nome completo");
+        nomeCliente = ler.nextLine();
+
+        System.out.println("Por favor digite o valor do seu saldo");
+        saldo = ler.nextDouble();
+       
+}
     }
 
